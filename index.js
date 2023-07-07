ef26db37a970c48bda1baf66643238f0de563218
Timothée Behra <timothee.behra@aworldforus.com>
2023-07-07 15:49:19 +0200

    make it an express server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const appPort = 3000;
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.render('./index.html');
+})
+
+app.listen(appPort, function () {
+  console.log(`Listening on port ${appPort}`);
+})
